f3efbb88d0160ecd0febd1b8cb7282978b4644d7
mj307 <m3dhavi@gmail.com>
2023-01-15 11:41:07 -0800

    Day 5 Part 1 Code

---
+++ day5part1.py
@@ -0,0 +1,31 @@
+stacks = [["R","N","F","V","L","J","S","M"],["P","N","D",'Z','F','J','W','H'], ["W",'R','C','D','G'],['N','B','S'],
+          ['M','Z','W','P','C','B','F','N'],['P','R','M','W'],['R','T','N','G','L','S','W'],['Q','T','H','F','N','B','V'],['L','M','H','Z','N','F']]
+allInstructions = []
+with open("day5input.txt") as fh:
+    for line in fh:
+        if line != "\n":
+            instructions = line.strip("\n").split(" ")
+            instructions.remove("move")
+            instructions.remove("from")
+            instructions.remove("to")
+            allInstructions.append(instructions)
+# how much you want to remove -- what stack to remove from -- what stack to put it in
+for a in allInstructions:
+    print (a)
+l = [9]
+print (l[-1])
+for eachInstruction in allInstructions:
+    amount = int(eachInstruction[0])
+    moveFrom = int(eachInstruction[1])
+    moveTo = int(eachInstruction[2])
+    for each in range(0,amount):
+        stacks[moveTo-1].append(stacks[moveFrom-1][-1])
+        stacks[moveFrom-1].pop()
+    print (stacks)
+    print ("######")
+
+message = ""
+for a in stacks:
+    message += a[-1]
+
+print (message)
