a9d91a6a87575dbe66935d17ea03b0015a7b1dff
mj307 <m3dhavi@gmail.com>
2023-01-20 16:09:50 -0800

    Day6 Part 1 Code

---
+++ day6part1.py
@@ -0,0 +1,37 @@
+character_list = []
+with open("day6input.txt") as fh:
+    for line in fh:
+        if line!="\n":
+            for a in line:
+                if a != "\n":
+                    character_list.append(a)
+
+print (character_list)
+mini_list = character_list[0:4]
+#print (mini_list)
+for character in range(3, len(character_list)):
+    print (mini_list)
+    temp_list = []
+    temp_var = 0
+
+    for a in mini_list:
+        if a in temp_list:
+            temp_var = 1
+        temp_list.append(a)
+    if temp_var == 1:
+        mini_list.remove(mini_list[0])
+        mini_list.append(character_list[character])
+    else:
+        print(character)
+        break
+
+
+
+'''
+    if character_list[character] in mini_list:
+        mini_list.remove(mini_list[0])
+        mini_list.append(character_list[character])
+        else:
+        print(character_list[character])
+        break
+'''
